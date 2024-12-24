67dc7f91dfee7bdd9e783af1bbf0fd2629fe1664
5ilent5pring <162632271+5ilent5pring@users.noreply.github.com>
2024-12-24 16:38:14 +0300

    Delete booleans_1.c

--- booleans_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int myAge = 25;
-  int votingAge = 18;
-  
-  printf("%d", myAge >= votingAge); // Returns 1 (true), meaning 25 year olds are allowed to vote!
-  return 0;
-}
