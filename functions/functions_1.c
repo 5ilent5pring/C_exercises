46308906b47809566c8b8eeedfd998d81fe8b6f8
5ilent5pring <162632271+5ilent5pring@users.noreply.github.com>
2024-12-25 09:59:59 +0300

    Update functions_1.c

--- functions/functions_1.c
+++ functions/functions_1.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
+// Function to convert Fahrenheit to Celsius
+float toCelsius(float fahrenheit) {
+  return (5.0 / 9.0) * (fahrenheit - 32.0);
+}
+
 int main() {
-  // Create a string
-  char fullName[30];
-
-  // Ask the user to input some text (full name)
-  printf("Type your full name and press enter: \n");
-
-  // Get the text
-  fgets(fullName, sizeof(fullName), stdin);
-
-  // Output the text
-  printf("Hello %s", fullName);
+  // Set a fahrenheit value
+  float f_value = 98.8;
   
+  // Call the function with the fahrenheit value
+  float result = toCelsius(f_value);
+
+  // Print the fahrenheit value
+  printf("Fahrenheit: %.2f\n", f_value);
+  
+  // Print the result
+  printf("Convert Fahrenheit to Celsius: %.2f\n", result);
+
   return 0;
 }
