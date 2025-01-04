2f1c62960ae0dc7ed9f59266baea969a9bb467da
5ilent5pring <162632271+5ilent5pring@users.noreply.github.com>
2025-01-04 20:16:59 +0300

    Create hello.c

---
+++ freestyle/hello.c
@@ -0,0 +1,19 @@
+/****************************************************
+*  hello ---> program to pprintout "Hello World!"   *
+*  not an especially earth-shattering program.      *
+*                                                   *
+*  Author: Sertaç Akman.                            *
+*                                                   *
+*  Purpose: Desmonstration of a single program      *
+*                                                   *
+*  Usage:                                           *
+*        Run the program and the message appears    *
+*****************************************************/
+
+#include <stdio.h>
+
+int main() {
+  /*Tell the world hello */
+  printf("Hello World!\n");
+  return (0);
+}
