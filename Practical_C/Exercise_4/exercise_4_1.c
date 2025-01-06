25ddcde554236fbfbcbfdc681c49981b1040be4d
5ilent5pring <162632271+5ilent5pring@users.noreply.github.com>
2025-01-06 10:04:44 +0300

    Update exercise_4_1.c

--- Practical_C/Exercise_4/exercise_4_1.c
+++ Practical_C/Exercise_4/exercise_4_1.c
@@ -2,9 +2,15 @@
 *  Write a program to print your name -, social security number, and date of birth    *
 ***************************************************************************************/
 #include <stdio.h>
-
+#include <stdint.h>
+//char Name[18];
+//int SecID;
+//char Birthdate[10];
 
 int main() {
-
-  return 0;
+        char Name[] = "Sertaç Akman";
+        int64_t SecID = 12345678910;
+        char Birthdate[] = "01.01.1991";
+        printf("%s has a %ld security number and birth date is %s\n", Name, SecID, Birthdate);
+        return 0;
 }
