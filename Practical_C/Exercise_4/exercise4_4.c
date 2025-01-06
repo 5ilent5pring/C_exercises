a74771640d54271e792d27cd0e294b62f0f3b518
5ilent5pring <162632271+5ilent5pring@users.noreply.github.com>
2025-01-06 11:57:13 +0300

    Create exercise4_4.c

---
+++ Practical_C/Exercise_4/exercise4_4.c
@@ -0,0 +1,4 @@
+/*****************************************************************************************
+* Write a program to print "HELLO" in big block letters,                                 * 
+* each letter should have a height of seven characters and width of five characters      *
+******************************************************************************************/
