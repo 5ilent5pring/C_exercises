769c6d96fd1b3ab12afb03f406860a1aa7301772
5ilent5pring <162632271+5ilent5pring@users.noreply.github.com>
2025-01-06 11:25:07 +0300

    Create exercise_4_2.c

---
+++ Practical_C/Exercise_4/exercise_4_2.c
@@ -0,0 +1,11 @@
+/***************************************************************
+* Write a program to print a block E using asteriks(*),        *
+* where the E has a height of seven characters                 *
+* and a width of five characters                               *
+****************************************************************/
+#include <stdio.h>
+
+int main() {
+        printf("*****\n*\n*\n***\n*\n*\n*****\n");
+        return 0;
+}
